12170c17ffbf9509e2c2bba034e348bcd90effca
willardshikami <willardshikami@gmail.com>
2017-02-09 01:47:37 +0300

    updated splash activity

--- app/src/main/java/com/example/willardpc/ngoroll/splashActivity.java
+++ app/src/main/java/com/example/willardpc/ngoroll/splashActivity.java
@@ -1,5 +1,6 @@
 package com.example.willardpc.ngoroll;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,22 @@ public class splashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread myThread = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        };
+
+        myThread.start();
     }
 }
