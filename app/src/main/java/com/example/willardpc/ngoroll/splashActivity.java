4fb9b7315f40f345cc4b87723d4d92740b3cea2d
willardshikami <willardshikami@gmail.com>
2017-03-13 10:10:24 +0300

    updated

--- app/src/main/java/com/example/willardpc/ngoroll/splashActivity.java
+++ app/src/main/java/com/example/willardpc/ngoroll/splashActivity.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 
 public class splashActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button buttonStart;
+    private TextView splashHeader;
+    private TextView splashParagraph;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,11 @@ public class splashActivity extends AppCompatActivity implements View.OnClickLis
         buttonStart = (Button)findViewById(R.id.buttonStart);
 
         buttonStart.setOnClickListener(this);
+
+
+        splashHeader = (TextView)findViewById(R.id.splashHeader);
+        splashParagraph = (TextView)findViewById(R.id.splashParagraph);
+
 
     }
 
